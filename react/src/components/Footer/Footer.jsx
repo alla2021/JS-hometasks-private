0789385c0d97b8ccd6240893d192ec61209b5b70
Alla <alla7gon@gmail.com>
2021-11-02 13:29:29 +0200

    react

---
+++ react/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => (
+    <footer className="footer">
+        <p className="footer__text">All rights reserved</p>
+    </footer>
+);
+
+export default Footer;
---
+++ react/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './navigation.scss';
+
+const Navigation = () => (
+  <nav className="nav">
+    <ul className="nav__list">
+      <li className="nav__item">Homepage</li>
+      <li className="nav__item">About</li>
+      <li className="nav__item">Contact</li>
+    </ul>
+  </nav>
+)
+
+
+export default Navigation
